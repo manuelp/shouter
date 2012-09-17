21e513af4333e25329d500f89a55b5277bcaa697
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-09-17 22:03:30 +0200

    init

---
+++ src/shouter/views/layout.clj
@@ -0,0 +1,22 @@
+(ns shouter.views.layout
+  (:use [hiccup.core :only [html]]
+        [hiccup.page :only [doctype include-css]]))
+
+(defn common [title & body]
+  (html (doctype :html5)
+        [:head
+         [:meta {:charset "urf-8"}]
+         [:meta {:http-equiv "X-UA-Compatible" :content "UE=edge,chrome=1"}]
+         [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
+         [:title title]
+         (include-css "/stylesheets/base.css")
+         (include-css "http://fonts.googleapis.com/css?family=Sigmar+One&v1")]
+        [:body
+         [:div {:id "header"}
+          [:h1 {:class "container"} "SHOUTER"]]
+         [:div {:id "content" :class "container"} body]]))
+
+(defn four-oh-four []
+  (common "Page Not Found"
+          [:div {:id "four-oh-four"}
+           "The page you requested could not be found"]))
